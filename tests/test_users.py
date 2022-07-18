371762c5b4318e2bba71dd32083292a0ecfc2dc1
ZephSibley <43991578+ZephSibley@users.noreply.github.com>
2022-07-18 16:09:21 +0100

    tests: Further logic tests for refreshing tokens

--- tests/test_users.py
+++ tests/test_users.py
@@ -15,21 +15,21 @@ class TestUsers(TestCase):
             user=self.user, provider='my-provider', uid='1234')
 
     def test_user_social_auth_actual_refresh_token_persists_after_calling_refresh_token(self):
+        new_access_token = '123'
+        refresh_token = 'abc'
+
+        self.usa.actual_refresh_token = refresh_token
+        self.usa.actual_access_token = '321'
+
         mock_backend = MagicMock()
         self.usa.get_backend_instance = MagicMock()
         self.usa.get_backend_instance.return_value = mock_backend
-        mock_backend.extra_data.return_value = {'access_token': '123'}
-
-        old_access_token = self.usa.actual_access_token
-        old_refresh_token = self.usa.actual_refresh_token
+        mock_backend.extra_data.return_value = {'access_token': new_access_token}
 
         self.usa.refresh_token(MagicMock())
 
-        new_access_token = '123'
-        new_refresh_token = self.usa.actual_refresh_token
-
-        self.assertEqual(old_refresh_token, new_refresh_token)
-        self.assertNotEqual(old_access_token, new_access_token)
+        self.assertEqual(refresh_token, self.usa.actual_refresh_token)
+        self.assertEqual(new_access_token, self.usa.actual_access_token)
 
     def test_access_token_is_set_to_none_if_access_token_is_not_present_in_response(self):
         mock_backend = MagicMock()
@@ -40,3 +40,22 @@ class TestUsers(TestCase):
         self.usa.refresh_token(MagicMock())
 
         self.assertIsNone(self.usa.actual_access_token)
+
+    def test_user_social_auth_actual_refresh_token_gets_set(self):
+        new_access_token = '123'
+        new_refresh_token = 'abc'
+
+        self.usa.actual_refresh_token = 'cba'
+        self.usa.actual_access_token = '321'
+
+        mock_backend = MagicMock()
+        self.usa.get_backend_instance = MagicMock()
+        self.usa.get_backend_instance.return_value = mock_backend
+        mock_backend.extra_data.return_value = {
+            'access_token': new_access_token,
+            'refresh_token': new_refresh_token}
+
+        self.usa.refresh_token(MagicMock())
+
+        self.assertEqual(new_refresh_token, self.usa.actual_refresh_token)
+        self.assertEqual(new_access_token, self.usa.actual_access_token)
